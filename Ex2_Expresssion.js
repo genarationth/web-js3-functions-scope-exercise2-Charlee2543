322ea63b68341159fbb514280c45b0d512379d59
Charlee2543 <Charlee2543@hotmail.com>
2024-05-15 11:07:20 +0700

    Ex2_Expresssion_and_Arrow-2Edit

--- Ex2_Expresssion.js
+++ Ex2_Expresssion.js
@@ -10,6 +10,8 @@ Ex2_number(4);
 
 // ### Exercise 2.2 converts the function from Exercise 2.1 into an arrow function</br>
 
-const Ex2_2_number = check2Number => check2Number&2===0 ?"number is even":"no number is even";
-const Answer = Ex2_2_number(7);
-console.log(Answer)
+const Ex2_2_number = Number => Number%2 === 0 ? "number is even" :"no number is even";
+console.log(Ex2_2_number(8))
+
+// const Answer = Ex2_2_number(9);
+// console.log(Answer)
